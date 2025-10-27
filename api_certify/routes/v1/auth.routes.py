4eeddfc98bc51085859a7e96f35a8bf17bca869d
dinho <claudiosilva.one@gmail.com>
2025-10-27 14:59:34 -0300

    refact: update-auth-routes

--- api_certify/routes/v1/auth.routes.py
+++ api_certify/routes/v1/auth.routes.py
@@ -3,11 +3,11 @@ from api_certify.schemas.responses import SucessResponse
 from api_certify.models.auth_model import AuthUser, AuthUserInDb
 from api_certify.service.auth_service import AuthService
 from api_certify.dependencies import get_auth_service
-auth_route = APIRouter(
+auth_routes = APIRouter(
   prefix='/auth', tags=["Auth"]
 )
 
-@auth_route.post('/', response_model=SucessResponse, status_code=201)
+@auth_routes.post('/', response_model=SucessResponse, status_code=201)
 async def create_auth(auth_data : AuthUser, service : AuthService = Depends(get_auth_service)):
     try:
       auth = await service.create_auth_user(auth_data)
