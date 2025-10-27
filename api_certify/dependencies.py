f9c8541faf0f0399b2bd59c2ec04a37b2b1a0c85
dinho <claudiosilva.one@gmail.com>
2025-10-27 18:05:20 -0300

    refact : update-dependencies

--- api_certify/dependencies.py
+++ api_certify/dependencies.py
@@ -1,11 +1,13 @@
-from api_certify.repositories.auth_repository import AuthRepository
+from fastapi import Depends
 from api_certify.service.auth_service import AuthService
+from api_certify.repositories.auth_repository import AuthRepository
 from api_certify.core.database.mongodb import db_mongo
 
-def get_auth_repository() -> AuthRepository:
-  database = await db_mongo.get_database()
-  return AuthRepository(database)
+async def get_auth_repository() -> AuthRepository:
+    database = await db_mongo.get_database()
+    return AuthRepository(database)
 
-def get_auth_service() -> AuthService:
-  repository = get_auth_repository()
-  return AuthService(repository)
+def get_auth_service(
+    repository: AuthRepository = Depends(get_auth_repository)
+) -> AuthService:
+    return AuthService(repository)
