e5ec1128eb8d2d394886006c34e3c6f7106fb067
dinho <claudiosilva.one@gmail.com>
2025-11-03 01:29:32 -0300

    refactor: add-auth-repository-to-certificate-service
    - Add AuthRepository as dependency in get_certificate_service
    - Update CertificateService to receive auth_repository
    - Reorganize functions order in dependencies file
    - Remove excessive whitespace

--- api_certify/dependencies.py
+++ api_certify/dependencies.py
@@ -11,19 +11,25 @@ async def get_auth_repository() -> AuthRepository:
     database = await db_mongo.get_database()
     return AuthRepository(database)
 
-
-async def get_certificate_repository() -> CertificateRepository:
-    database = await db_mongo.get_database()
-    return CertificateRepository(database)
-
-
 def get_auth_service(
     auth_repository: AuthRepository = Depends(get_auth_repository),
 ) -> AuthService:
     return AuthService(auth_repository)
 
+     # primeira mudança: 
+    # Adciona corretamente aqui a injeção de dependência do AuthRepository para o CertificateService
+    # injetei também o AuthRepository aqui
+    
+async def get_certificate_repository() -> CertificateRepository:
+    database = await db_mongo.get_database()
+    return CertificateRepository(database)
+
+
 
 def get_certificate_service(
     certificate_repository: CertificateRepository = Depends(get_certificate_repository),
+    auth_repository : AuthRepository = Depends(get_auth_repository),
 ) -> CertificateService:
-    return CertificateService(certificate_repository)
+    return CertificateService(certificate_repository, auth_repository)
+
+
