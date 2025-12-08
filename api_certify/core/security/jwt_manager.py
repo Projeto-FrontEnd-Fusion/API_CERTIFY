cebdc2f785d899bfbe11811c5b2259cfb35ef946
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-12-08 19:44:55 -0300

    fix: rename environment variable.

--- api_certify/core/security/jwt_manager.py
+++ api_certify/core/security/jwt_manager.py
@@ -12,7 +12,7 @@ from api_certify.models.jwt_model import TokenData, Token
 
 load_dotenv()
 
-JWT_SECRET_KEY = os.getenv("SECRET_KEY")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
 ACCESS_TOKEN_EXPIRES = int(os.getenv("ACCESS_TOKEN_EXPIRES"))
 
