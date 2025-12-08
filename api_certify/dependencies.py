db07a2c4f5daadec37635e8ae58a3f9a3dd7fcf3
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-12-08 15:59:54 -0300

    feat: add authentication dependencies.

--- api_certify/dependencies.py
+++ api_certify/dependencies.py
@@ -1,10 +1,27 @@
-from fastapi import Depends
+from typing import List
+from bson import ObjectId
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 
 from api_certify.core.database.mongodb import db_mongo
+from api_certify.models.auth_model import AuthUserReponse
 from api_certify.service.auth_service import AuthService
 from api_certify.repositories.auth_repository import AuthRepository
 from api_certify.service.certificate_service import CertificateService
 from api_certify.repositories.certificate_repository import CertificateRepository
+
+from api_certify.models.jwt_model import TokenData
+from api_certify.core.security.jwt_manager import JWTManager
+
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="login",
+    description=(
+        "Use um token JWT no formato:\n\n"
+        "`Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.EXEMPLO123.ABCD`"
+    )
+)
 
 
 async def get_auth_repository() -> AuthRepository:
@@ -33,3 +50,35 @@ def get_certificate_service(
     return CertificateService(certificate_repository, auth_repository)
 
 
+def get_current_user(token: str = Depends(oauth2_scheme)) -> TokenData:
+    payload = JWTManager.verify_and_decode_jwt(token)
+    return payload
+
+
+async def get_current_active_user(current_user: TokenData = Depends(get_current_user)) -> AuthUserReponse:
+    database = await db_mongo.get_database()
+    user = await database.auth_database.find_one({"_id": ObjectId(current_user.sub)})
+
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="Usuário não encontrado"
+            )
+
+    if user.get("status", "").lower() != "active":
+        raise HTTPException(
+            status_code=401,
+            detail="Usuário não autenticado"
+            )
+    return user
+
+
+def require_roles(allowed_roles: List[str]):
+    def dependency(current_user=Depends(get_current_active_user)):
+        if current_user.get("role") not in allowed_roles:
+            raise HTTPException(
+                status_code=403,
+                detail="Permissões insuficientes"
+                )
+        return current_user
+    return dependency
