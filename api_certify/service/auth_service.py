36f51dd7ca254ee6a0ca353fba8d418af8bd212a
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-12-08 15:58:52 -0300

    feat: add authentication service.

--- api_certify/service/auth_service.py
+++ api_certify/service/auth_service.py
@@ -3,16 +3,37 @@ from api_certify.models.auth_model import (
   AuthUser, 
   AuthUserInDb, 
   AuthUserLogin,
-  AuthUserReponse
+  AuthUserReponse,
+  Role
 )
+
+from api_certify.models.jwt_model import Token, TokenData, TokenResponse
+from api_certify.core.security.jwt_manager import JWTManager
+
+
 class AuthService:
-  def __init__(self, auth_repository : AuthRepository):
+  def __init__(self, auth_repository : AuthRepository, jwt_manager: JWTManager):
     self.auth_repository = auth_repository
-  
+    self.jwt_manager = jwt_manager
+
   async def create_auth_user(self, auth_data : AuthUser) -> AuthUserReponse:
     auth = await self.auth_repository.create(auth_data)
     return auth
-  
-  async def login_auth(self, auth_data: AuthUserLogin) -> AuthUserReponse:
+
+  async def login_auth(self, auth_data: AuthUserLogin) -> TokenResponse:
     auth = await self.auth_repository.login(auth_data)
-    return auth
+    token = await self.generate_token(auth)
+    response = {
+      "user": auth,
+      "access_token": token.access_token,
+      "token_type": "bearer"
+    }
+    return response
+
+  async def generate_token(self, data: AuthUserReponse) -> Token:
+    token_data = TokenData(
+      sub=data.id,
+      role=Role(data.role)
+    )
+    token = await self.jwt_manager.create_jwt(token_data)
+    return token
