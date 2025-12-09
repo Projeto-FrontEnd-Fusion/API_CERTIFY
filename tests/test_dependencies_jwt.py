4eb45adbb1eb3ab517531e22c3e28d90f010bab1
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-12-08 22:12:34 -0300

    fix: Removed unit tests for auth_service and modified dependency tests.

--- tests/test_dependencies_jwt.py
+++ tests/test_dependencies_jwt.py
@@ -9,11 +9,13 @@ from api_certify.models.jwt_model import TokenData
 
 def test_get_current_user():
     fake_token = "fake.jwt.token"
-    fake_payload = TokenData(sub="123", role="user")
-    
-    with patch.object(JWTManager, "verify_and_decode_jwt", return_value=fake_payload):
+    fake_token_data = TokenData(sub="123", role="user")
+
+    # Ajuste: faça o mock retornar um dict
+    with patch("api_certify.dependencies.JWTManager.verify_and_decode_jwt", return_value=fake_token_data.dict()):
         result = get_current_user(fake_token)
-        assert result == fake_payload
+        assert result["sub"] == "123"
+        assert result["role"] == "user"
 
 
 @pytest.mark.asyncio
@@ -28,7 +30,7 @@ async def test_get_current_active_user():
         "status": "active"
     }
 
-    fake_token_data = TokenData(sub="64b8c8d1f1e2a3b4c5d6e7f8", role="user")
+    fake_token_data = { "sub":"64b8c8d1f1e2a3b4c5d6e7f8", "role":"user" }
     
     mock_db = AsyncMock()
     mock_db.auth_database.find_one.return_value = fake_user
